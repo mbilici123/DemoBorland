7ce951d6087992c34feadcf45257feb3adf5a045
mbilici123 <41476563+mbilici123@users.noreply.github.com>
2018-07-24 14:33:29 +0300

    test.java
    deneme

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -11,7 +11,8 @@ public class test extends BaseTest {
 
     }
     WebDriver driver = new ChromeDriver();
-    String mailRandom = "mbilici" + createRandomNumber(5) + "@gmail.com";
+    String mailRandom = "tiwoyalivu" + createRandomNumber(6) + "@fxprix.com";
+    String password = createRandomNumber(10);
     public test openPage() {
         System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
         driver.get("http://demo.borland.com/InsuranceWebExtJS/");
@@ -43,31 +44,32 @@ public class test extends BaseTest {
         driver.findElement(By.id("login-form:signup")).click();
         driver.findElement(By.id("signup:fname")).sendKeys("Mertcan");
         driver.findElement(By.id("signup:lname")).sendKeys("Bilici");
-        driver.findElement(By.id("BirthDate")).sendKeys("18.08.08");
+        driver.findElement(By.id("BirthDate")).sendKeys("28/08/2001");
         driver.findElement(By.id("signup:email")).sendKeys(mailRandom);
+        System.out.println("E-mail; " + mailRandom);
         driver.findElement(By.id("signup:street")).sendKeys("asfdsa");
         driver.findElement(By.id("signup:city")).sendKeys("asfdsa");
         driver.findElement(By.id("signup:state")).sendKeys("Texas");
-        driver.findElement(By.id("signup:zip")).sendKeys("4314");
-        driver.findElement(By.id("signup:password")).sendKeys("qwe");
+        driver.findElement(By.id("signup:zip")).sendKeys("5543");
+        driver.findElement(By.id("signup:password")).sendKeys(password);
+        System.out.println("Şifre; " + password);
         driver.findElement(By.id("signup:signup")).click();
         driver.findElement(By.id("home")).click();
         driver.findElement(By.id("logout-form:logout")).click();
         driver.findElement(By.id("login-form:email")).sendKeys(mailRandom);
-        driver.findElement(By.id("login-form:password")).sendKeys("qwe");
+        driver.findElement(By.id("login-form:password")).sendKeys(password);
         driver.findElement(By.id("login-form:login")).click();
+     //   driver.findElement(By.id("logout-form:logout")).click();
         return this;
     }
 
 
 
     @Test(enabled = true, description = "TS0001 ghgh")
-    public void TS0001() throws InterruptedException{
-
-        openPage();
-        signUp();
-        }
-
+    public void TS0001() throws InterruptedException {
+            openPage();
+            signUp();
+    }
 }
 
 
